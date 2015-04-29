d8af52ceb33c5c27e2675d3b6c5acc186d2ed7a9
Guilherme Klink <guilherme@yahoo.com>
2015-04-28 22:34:15 -0500

    Changed comments

--- csv_handler.py
+++ csv_handler.py
@@ -2,17 +2,21 @@
 
 
 from data_util import *
-
+import csv
 
 ################ CONVENTIONS #################
 ##############################################
 #          Array of DataRow = table			 #
+#            Data Row = Instance             #
 ##############################################
 
 # IMPORT CSV DATA ##############################################################################
 
 # Gets a string path for a CSV file and returns an array of DataRow (Table), where each position of the array
 # will correpspond to a table line
+
+# Entry ->
+# Returns ->
 
 def importDataCSV(path):		
 	return 0
@@ -23,6 +27,9 @@ def importDataCSV(path):
 # DATA FILTER #################################################################################
 
 # Given a table, a feature and a value returns a table containing entries with that value
+
+# Entry ->
+# Returns ->
 
 def filterTable(table, feature, value):		
 	return 0
