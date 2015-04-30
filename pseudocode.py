3d03fe053898599feb7e065997218e42038a56b1
Mahdieh Nejati Javaremi <m.nejati@u.northwestern.edu>
2015-04-30 08:16:35 -0500

    minor notes to self

---
+++ pseudocode.py
@@ -0,0 +1,15 @@
+Tests for splits: 
+
+For Discrete Attributes: 
+Is A equal to blah
+
+For Continuous Attributes:
+threshold = the cases in Nm are sorted on their values (distinctAttributes)
+gives ordered liste of distinct values v_i, ... v_n. 
+For every pair of adjacent valuess suggest a potential threshold t = (vi + vi+1)/2
+and choose threshold that yeild best value of the splitting criterion 
+selected. 
+Is A less than or equal to threshold
+
+
+Splitting criterion: Gain ratio
--- tree_util.py
+++ tree_util.py
@@ -4,8 +4,25 @@
 # Node ######################################################################################
 
 class Node():
-	def __init__():
-		pass
+	def __init__(self, tag, parent, DataRowID, children, splitType, value, attribute):
+		self.tag = tag
+		self.parent = parent
+		self.DataRowID = DataRowID
+		self.children = children
+		self.splitType = splitType
+		self.value = value
+		self.attribute = attribute
+
+		### Add self.max and self.min for logic rules???
+
+		phi = 0.1 #??????? COME BACK TO THIS
+
+		def isLeaf(self):
+			if NodeEntropy(self.tag) < phi  # If split is pure enough
+				leaf = 1		# mark node as leaf to prevent further spliting
+			else leaf = 0		# mark node as non-leaf for further splitting
+
+			return leaf
 
 		
 ###############################################################################################
@@ -24,14 +41,16 @@ def BestSplit():
 
 ###############################################################################################
 
-# ENTROPHY ####################################################################################
+# NODE ENTROPY ####################################################################################
 
 # Def
 
 # Entry ->
 # Returns ->
 
-def BestSplit():		
+def NodeEntropy(tag):
+	Nm = filterTableByID(table, idList, )	
+
 	return 0
 
 
@@ -52,4 +71,17 @@ def SplitEntrophy():
 ###############################################################################################
 
 
+# Retrieve Node ##############################################################################
 
+# Function that returns a single node
+
+# Entry -> DataRow[]
+# Returns -> 
+
+def RetrieveNode():	
+	pass
+		
+	for row in table:
+		print row.toStringCSV()
+
+##############################################################################################
